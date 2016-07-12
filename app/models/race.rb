41f7711dffe9475ed1f970e28c5c2211d289d087
隗云志 <yunzhi.wei@innonetwork.com>
2016-07-12 17:59:50 +0800

    $ rspec spec/service_facade_spec.rb -e rq01

--- app/models/race.rb
+++ app/models/race.rb
@@ -5,6 +5,7 @@ class Race
   field :n,    as: :name,     type: String
   field :date, as: :date,     type: Date
   field :loc,  as: :location, type: Address
+  field :next_bib,            type: Integer,  default: 0
 
   embeds_many :events, as: :parent, order: [:order.asc]
   has_many :entrants, class_name: 'Entrant', foreign_key: "race._id", dependent: :delete, order: [:secs.asc, :bib.asc]
@@ -78,4 +79,7 @@ class Race
   	end
   end
 
+  def next_bib
+    self[:next_bib] += 1
+  end
 end
