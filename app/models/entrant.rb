0aa777f2abca0b1d564f7d9436058c87066c3da3
隗云志 <yunzhi.wei@innonetwork.com>
2016-07-12 17:25:41 +0800

    $ rspec spec/flatten_attributes_spec.rb -e rq04 $ rspec spec/flatten_attributes_spec.rb

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -25,7 +25,7 @@ class Entrant
   delegate :date,       :date=,       to: :race, prefix: "race"
 
   def update_total(result)
-  	self.secs=results.inject(0) {|sum, element| sum + element.secs }
+  	self.secs=results.inject(0) {|sum, element| sum + (element.secs ? element.secs : 0) }
   end
 
   def the_race
@@ -44,4 +44,41 @@ class Entrant
   def group_name
     group.name if group
   end
+
+  RESULTS = {
+    "swim" => SwimResult,
+    "t1"   => LegResult,
+    "bike" => BikeResult,
+    "t2"   => LegResult,
+    "run"  => RunResult
+  }
+
+  RESULTS.keys.each do |name|
+    # create_or_find result
+    define_method("#{name}") do
+      result=results.select {|result| name==result.event.name if result.event}.first
+      if !result
+        result=RESULTS["#{name}"].new(:event=>{:name=>name})
+        results << result
+      end
+      result
+    end
+
+    # assign event details to result
+    define_method("#{name}=") do |event|
+      event=self.send("#{name}").build_event(event.attributes)
+    end
+
+    # expose setter/getter for each property of each result
+    RESULTS["#{name}"].attribute_names.reject {|r| /^_/===r}.each do |prop|
+      define_method("#{name}_#{prop}") do
+        event=self.send(name).send(prop)
+      end
+      define_method("#{name}_#{prop}=") do |value|
+        event=self.send(name).send("#{prop}=", value)
+        update_total nil if /secs/===prop
+      end
+    end
+  end
+
 end
