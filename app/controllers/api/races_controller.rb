74f25d784f51addb767cb1b5098cadc9121069ab
隗云志 <yunzhi.wei@innonetwork.com>
2016-07-18 19:50:26 +0800

    $ rspec spec/uri_spec.rb -e rq03

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -44,47 +44,5 @@ module Api
 
   end
 
-  class RacersController < ApplicationController
-
-    def index
-      if !request.accept || request.accept == "*/*"
-        render plain: "/api/racers" 
-      else
-        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:id]}"
-      end
-    
-    end
-  
-    def show
-      if !request.accept || request.accept == "*/*"
-        render plain: "/api/racers/#{params[:id]}" 
-      else
-        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:id]}"
-      end
-    end
-
-  end
-
-  class EntriesController < ApplicationController
-
-    def index
-      if !request.accept || request.accept == "*/*"
-        render plain: "/api/racers/#{params[:racer_id]}/entries" 
-      else
-        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:racer_id]}, entry_id - #{params[:id]}"
-      end
-      
-    end
-  
-    def show
-      if !request.accept || request.accept == "*/*"
-        render plain: "/api/racers/#{params[:racer_id]}/entries/#{params[:id]}" 
-      else
-        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:racer_id]}, entry_id - #{params[:id]}"
-      end
-      
-    end
-
-  end
 
 end
