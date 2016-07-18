13483d9c15c06553570038bd1ce970d886e02c3a
隗云志 <yunzhi.wei@innonetwork.com>
2016-07-18 19:31:03 +0800

    Unit test fail: $ rspec spec/uri_spec.rb -e rq03

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -2,56 +2,87 @@ module Api
 
   class RacesController < ApplicationController
 
-    # GET /racers
-    # GET /racers.json
     def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}"
+      end
+      
     end
   
-    # GET /racers/1
-    # GET /racers/1.json
     def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:id]}" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, race_id - #{params[:id]}"
+      end
+      
     end
 
   end
 
   class ResultsController < ApplicationController
 
-    # GET /racers
-    # GET /racers.json
     def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:race_id]}/results" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, race_id - #{params[:race_id]}, result_id - #{params[:id]}"
+      end
+      
     end
   
-    # GET /racers/1
-    # GET /racers/1.json
     def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, race_id - #{params[:race_id]}, result_id - #{params[:id]}"
+      end
+      
     end
 
   end
 
   class RacersController < ApplicationController
 
-    # GET /racers
-    # GET /racers.json
     def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/racers" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:id]}"
+      end
+    
     end
   
-    # GET /racers/1
-    # GET /racers/1.json
     def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/racers/#{params[:id]}" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:id]}"
+      end
     end
-  
+
   end
 
   class EntriesController < ApplicationController
 
-    # GET /racers
-    # GET /racers.json
     def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/racers/#{params[:racer_id]}/entries" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:racer_id]}, entry_id - #{params[:id]}"
+      end
+      
     end
   
-    # GET /racers/1
-    # GET /racers/1.json
     def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/racers/#{params[:racer_id]}/entries/#{params[:id]}" 
+      else
+        render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, racer_id - #{params[:racer_id]}, entry_id - #{params[:id]}"
+      end
+      
     end
 
   end
