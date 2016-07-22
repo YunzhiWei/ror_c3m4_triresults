cf5a59ba2366b3787b99d7d5abe4dccf82b420d6
隗云志 <yunzhi.wei@innonetwork.com>
2016-07-22 18:12:02 +0800

    To compatible with $ rspec spec/uri_spec.rb -e rq03

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -25,12 +25,12 @@ module Api
   
     def show
       
-      @result=Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
-
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}" 
       else
         # render plain:  "api: controller - #{params[:controller]}, action - #{params[:action]}, race_id - #{params[:race_id]}, result_id - #{params[:id]}"
+        
+        @result=Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
         render :partial=>"result", :object=>@result
       end
       
